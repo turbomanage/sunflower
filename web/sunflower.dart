10bba21f077f4c269298c50afe1f347b084fc5d1
David M. Chandler <turbomanage@gmail.com>
2016-04-05 00:42:31 -0600

    self-growing version

--- web/sunflower.dart
+++ web/sunflower.dart
@@ -6,22 +6,27 @@ library sunflower;
 
 import 'dart:html';
 import 'dart:math';
+import 'dart:async';
 
 const String ORANGE = "orange";
 const int SEED_RADIUS = 2;
 const int SCALE_FACTOR = 4;
 const num TAU = PI * 2;
-const int MAX_D = 300;
+const int MAX_D = 500;
 const num centerX = MAX_D / 2;
 const num centerY = centerX;
+const delay = const Duration(milliseconds:5);
 
 final InputElement slider = querySelector("#slider");
 final InputElement phiSlider = querySelector("#phi_slider");
+final ButtonElement startButton = querySelector("#start");
 final Element notes = querySelector("#notes");
 final Element theta = querySelector("#theta");
 final num PHI = (sqrt(5) + 1) / 2;
 int seeds = 0;
+int n = 0;
 num phi = PHI;
+bool growing = false;
 
 final CanvasRenderingContext2D context =
   (querySelector("#canvas") as CanvasElement).context2D;
@@ -33,7 +38,8 @@ void main() {
   phiSlider.onChange.listen((e) => draw());
   phiSlider.onMouseMove.listen((e) => draw());
   phiSlider.onKeyDown.listen((e) => draw());
-  draw();
+  startButton.onClick.listen((e) => grow());
+//  draw();
 }
 
 /// Draw the complete figure for the current number of seeds.
@@ -42,17 +48,46 @@ void draw() {
   phi = int.parse(phiSlider.value) / 10000;
   context.clearRect(0, 0, MAX_D, MAX_D);
   for (var i = 0; i < seeds; i++) {
-    final num theta = i * TAU / phi;
-    final num r = sqrt(i) * SCALE_FACTOR;
-    drawSeed(centerX + r * cos(theta), centerY - r * sin(theta));
+    drawSeed(i);
   }
   drawEnclosingArc();
   notes.text = "${seeds} seeds";
   theta.text = "${phi} = phi";
 }
 
+void grow() {
+  growing = true;
+  seeds = int.parse(slider.value);
+  phi = int.parse(phiSlider.value) / 10000;
+  context.clearRect(0, 0, MAX_D, MAX_D);
+  n = 1;
+  new Timer.periodic(delay, (Timer t) => growSeed(t));
+//  Future.doWhile(growSeed);
+//    window.animationFrame(() => drawSeed(i));
+  drawEnclosingArc();
+  notes.text = "${seeds} seeds";
+  theta.text = "${phi} = phi";
+}
+
+void drawSeed(int i) {
+  final num theta = i * TAU / phi;
+  final num r = sqrt(i) * SCALE_FACTOR;
+  drawSeedAt(centerX + r * cos(theta), centerY - r * sin(theta));
+}
+
+bool growSeed(Timer t) {
+  final num theta = n * TAU / phi;
+  final num r = sqrt(n) * SCALE_FACTOR;
+  drawSeedAt(centerX + r * cos(theta), centerY - r * sin(theta));
+    if (n < seeds) {
+      n++;
+    } else {
+      t.cancel();
+    }
+}
+
 /// Draw a small circle representing a seed centered at (x,y).
-void drawSeed(num x, num y) {
+void drawSeedAt(num x, num y) {
   context..beginPath()
          ..lineWidth = 2
          ..fillStyle = ORANGE
